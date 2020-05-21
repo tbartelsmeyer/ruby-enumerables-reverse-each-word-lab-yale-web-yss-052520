330305d38c3b7712ae049a2216517eec355ccc0c
Tilman Bartelsmeyer <tilman.bartelsmeyer@yale.edu>
2020-05-21 05:54:14 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,11 +1,10 @@
 def reverse_each_word(string)
   out_arr = string.split(/ /).collect { |word|
     word_arr = word.split(//)
-    out_pos = word_arr.length - 1
-    out_word = []
-    word_arr.map { |char|
-      out_word[out_pos] = char
-      out_pos -= 1
+    reverse_pos = word_arr.length - 1
+    out_word = word_arr.map {
+      word_arr[reverse_pos]
+      reverse_pos -= 1
     }
     out_str = out_word.join
   }
@@ -16,7 +15,7 @@ out = reverse_each_word("This is a test")
 puts "final out: #{out}"
 
 
-def reverse_each_word(string)
+def reverse_each_word_each(string)
   out_arr = string.split(/ /).collect { |word|
     word_arr = word.split(//)
     out_pos = word_arr.length - 1
