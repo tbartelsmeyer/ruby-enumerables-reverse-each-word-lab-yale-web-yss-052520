348e8fef8576ff50bcae4e8fde1b0c7e1c48b547
Tilman Bartelsmeyer <tilman.bartelsmeyer@yale.edu>
2020-05-21 05:49:50 +0000

    Done.

---
+++ reverse_each_word.rb
@@ -0,0 +1,13 @@
+def reverse_each_word(string)
+  out_arr = string.split(/ /).collect { |word|
+    word_arr = word.split(//)
+    out_pos = word_arr.length - 1
+    out_word = []
+    word_arr.each { |char|
+      out_word[out_pos] = char
+      out_pos -= 1
+    }
+    out_str = out_word.join
+  }
+  out_arr.join(" ")
+end
