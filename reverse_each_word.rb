66db2847c0744572b393e3f350830ea9c14d9abc
Tilman Bartelsmeyer <tilman.bartelsmeyer@yale.edu>
2020-05-21 05:57:01 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,10 +1,10 @@
 def reverse_each_word(string)
   out_arr = string.split(/ /).collect { |word|
     word_arr = word.split(//)
-    reverse_pos = word_arr.length - 1
+    reverse_pos = word_arr.length
     out_word = word_arr.map {
-      word_arr[reverse_pos]
       reverse_pos -= 1
+      word_arr[reverse_pos]
     }
     out_str = out_word.join
   }
